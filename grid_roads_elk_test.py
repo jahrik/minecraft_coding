c85998f7ee0783fdb44d7bbb83ed34e4dac7d577
Christopher B. Kolp <reservedwickedshadow@gmail.com>
2019-11-14 17:53:51 -0700

    Add erin's test file.

--- grid_roads_elk_test.py
+++ grid_roads_elk_test.py
@@ -1,6 +1,8 @@
 from mcpi.minecraft import Minecraft
 from random import randrange
+
 mc = Minecraft.create()
+
 random_gm = randrange(1, 100) #gm = gridmap
 #gm_1 setup
 gm_1_mod_1 = randrange(1, 3)
@@ -33,20 +35,34 @@ def createRoad(length, direction):
             x = x - 4
         else:
             exit(1)
+        
         loops = loops + 1
 
-mc.player.setPos(0, -30, 0)
-mc.setBlocks(120, 63, 120, -120, -30, -120, 0)#clear map
-x, y, z = mc.player.getPos()
-mc.setBlocks(120, y-1, 120, -120, y-1, -120, 2)#grass
+def createIntersection():
+    x, y, z = mc.player.getPos()
 
-#grid_1 road building
-mc.setBlocks(x+5, y-1, z+5, x-5, y-1, z-5, 35, 15)
-mc.setBlocks(x+5, y-1, z+5, x+3, y-1, z+3, 43)
-mc.setBlocks(x-5, y-1, z-5, x-3, y-1, z-3, 43)
-mc.setBlocks(x+5, y-1, z-5, x+3, y-1, z-3, 43)
-mc.setBlocks(x-5, y-1, z+5, x-3, y-1, z+3, 43)
-mc.setBlocks(x+3, y-1, z+3, x-3, y-1, z-3, 35, 15)
+    mc.setBlocks(x+5, y-1, z+5, x-5, y-1, z-5, 35, 15)
+    mc.setBlocks(x+5, y-1, z+5, x+3, y-1, z+3, 43)
+    mc.setBlocks(x-5, y-1, z-5, x-3, y-1, z-3, 43)
+    mc.setBlocks(x+5, y-1, z-5, x+3, y-1, z-3, 43)
+    mc.setBlocks(x-5, y-1, z+5, x-3, y-1, z+3, 43)
+    mc.setBlocks(x+3, y-1, z+3, x-3, y-1, z-3, 35, 15)
+
+def clearMap():
+    # Clear the map of all blocks
+    mc.setBlocks(120, 63, 120, -120, -30, -120, 0)
+    # Determine relative position
+    x, y, z = mc.player.getPos()
+    # Add a grass block layer
+    mc.setBlocks(120, y-1, 120, -120, y-1, -120, 2)
+
+
+mc.player.setPos(60, -30, 60)
+clearMap()
+createIntersection()
+
+x, y, z = mc.player.getPos()
+
 mc.player.setPos(x+5, y, z)
 createRoad(4, "East")
 mc.player.setPos(x-8, y, z)
@@ -55,6 +71,8 @@ mc.player.setPos(x, y, z+5)
 createRoad(4, "North")
 mc.player.setPos(x, y, z-8)
 createRoad(4, "South")
+
+
 #gridMap_1_module_1
 #if gm_1_mod_1 == 1:
 #    mc.player.setPos(x+5, y, z)
